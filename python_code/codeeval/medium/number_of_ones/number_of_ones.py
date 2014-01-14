1b0f714a07ae076a9d48c51d7be14462a57fbeec
donald-ngo <donald.ngo@hp.com>
2014-01-14 12:42:51 -0800

    upload python code

---
+++ python_code/codeeval/medium/number_of_ones/number_of_ones.py
@@ -0,0 +1,23 @@
+import sys
+import math
+
+try:
+    test_cases = open(sys.argv[1], 'r')
+except:
+    raise Exception("Cannot open file!")
+
+test = ""
+log2value =  0.0
+number = 0
+#10 = 101 2^3=9 + 2^0=1 =  10 
+for test in test_cases:
+    number = int(test)
+    print "number:", number
+    log2value = math.log( number ) / math.log( 2 )
+    floating_value = log2value - int(log2value)
+    integer_value = int(log2value)
+    print "floating_value:", floating_value
+    print "integer_value:", integer_value
+test_cases.close()
+
+
