5698953871e0bb2266cabfe152315ef111660143
Donald Ngo <donald.ngo@hp.com>
2014-03-24 17:18:38 -0700

    add shared_resources folder

---
+++ shared_resources/start_selenium_RCs.bat
@@ -0,0 +1,6 @@
+start "Selenium 4444" /min java.exe -jar c:\selenium\server\selenium-server-standalone-2.0b3.jar -port 4444 
+start "Selenium 4445" /min java.exe -jar c:\selenium\server\selenium-server-standalone-2.0b3.jar -port 4445 
+start "Selenium 4446" /min java.exe -jar c:\selenium\server\selenium-server-standalone-2.0b3.jar -port 4446 
+start "Selenium 4447" /min java.exe -jar c:\selenium\server\selenium-server-standalone-2.0b3.jar -port 4447 
+
+
