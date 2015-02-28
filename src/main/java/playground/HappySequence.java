670d5fe0f3717a4dc2565d3f46ed2e44df3ff531
donald-ngo <donaldngo@aol.com>
2015-02-27 16:06:11 -0800

    Fixed warnings.

--- src/main/java/playground/HappySequence.java
+++ src/main/java/playground/HappySequence.java
@@ -6,7 +6,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 
 public class HappySequence {
 	public static int getLongestHappy(String filePath) throws IOException{
