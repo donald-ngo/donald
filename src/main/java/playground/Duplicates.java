abed8533feb617881d6e554cc7f03716ef30e1dd
donald-ngo <donaldngo@aol.com>
2015-03-16 17:34:04 -0700

    Modified Duplicates.java

--- src/main/java/playground/Duplicates.java
+++ src/main/java/playground/Duplicates.java
@@ -9,24 +9,37 @@ public class Duplicates {
 		int numbers[] = {-9, 1, 2, 55, 6, 10, 4, 5, 6, -9, 1, 2, 1, 1, -9};
 
 		HashSet<Integer> numbersSet = new HashSet<Integer>();
-		HashMap<Integer,Integer> duplicateNumbers = new HashMap<Integer,Integer>();
+		HashMap<Integer,Integer> duplicateNumbersMap = new HashMap<Integer,Integer>();
+		
+		String uniqueNumbers = "";
 		
 		//Loop through the numbers array
 		for (int i = 0; i <numbers.length; i++){
 			if ( !numbersSet.contains(numbers[i]) ){
 				numbersSet.add(numbers[i]);
+				uniqueNumbers += " " + numbers[i] + ",";
 			}
 			else{ //The number is a duplicate
-				if (!duplicateNumbers.containsKey(numbers[i])){//First duplicate, initialize to be 2 occurrences
-					duplicateNumbers.put(numbers[i],2); 
+				if (!duplicateNumbersMap.containsKey(numbers[i])){//First duplicate, initialize to be 2 occurrences
+					duplicateNumbersMap.put(numbers[i],2); 
 				}
 				else{
-					duplicateNumbers.put(numbers[i],duplicateNumbers.get(numbers[i])+1); //We see the number duplicated again
+					duplicateNumbersMap.put(numbers[i],duplicateNumbersMap.get(numbers[i])+1); //We see the number duplicated again
 				}
 			}		
 		}
+			
+		String duplicateNumbers = "";
+		for (int numberKey : duplicateNumbersMap.keySet() ){
+			duplicateNumbers += " " + numberKey +  " appears " +   duplicateNumbersMap.get(numberKey) + " times,";
+		}
+		
+		uniqueNumbers = uniqueNumbers.substring(0, uniqueNumbers.length()-1);
+		duplicateNumbers = duplicateNumbers.substring(0, duplicateNumbers.length()-1);
+		
+		
 		System.out.println("Original numbers: " + Arrays.toString(numbers));
-		System.out.println("Set of unique numbers: " + numbersSet);
+		System.out.println("Unique numbers: " + uniqueNumbers);
 		System.out.println("Duplicate numbers: " + duplicateNumbers);
 	}
 
