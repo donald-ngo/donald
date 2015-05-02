04849e74520b127bacad22f24773dfe3eee73b61
donald-ngo <donaldngo@aol.com>
2015-05-02 12:10:17 -0700

    First excercise.

---
+++ src/main/java/structures/MyFirstProgram.java
@@ -0,0 +1,18 @@
+package structures;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MyFirstProgram {
+
+	public static void main (String[] arguments){
+		List<String> stooges = new ArrayList<String>(Arrays.asList("Larry", "Moe", "Curly"));
+		System.out.println(stooges);
+		stooges.add(1,"Donald");
+		System.out.println(stooges);
+		System.out.println(stooges.getClass());
+		
+	}
+
+}
